50e0b5bd9b8200a8362b3b2d8093dfb305ff261d
design <a0104design@gmail.com>
2026-04-01 17:35:00 +0900

    count 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 function App() {
-  return <></>
+    // statr라는 측수 변수를 만들 때 useState();useState(); 사용
+    const [count, setCount] = useState(100);
+    return (
+        <div>
+            <h1>Counter : {count}</h1>
+            {/* 태그의 속성을 적어중줄때 그 안에 자바스크립트를 작성해야한다면 "" 로 써 주는 게 아니라 {} 로 써 줘야 함
+          ==> 컴포넌트 문 안에 자바스크립트를 쓰겠다는 의미 */}
+            <button
+                onClick={() => {
+                    // 카운트 라고 하는 변수에 지금 현재 카운트의 값 - 1의 값이 저장되면 됨
+                    //count = count - 1;(일반 변수라면)
+                    setCount(count - 1);
+                }}>
+                -
+            </button>
+            <button
+                onClick={() => {
+                    setCount(0);
+                }}>
+                Reset
+            </button>
+            <button
+                onClick={() => {
+                    setCount(count + 1);
+                }}>
+                +
+            </button>
+        </div>
+    );
 }
 
 export default App;
